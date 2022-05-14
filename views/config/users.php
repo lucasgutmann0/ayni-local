9b1891815d9550215e9ed2cd2b4d7f8c98b37969
gutmanndev <lucas.garcia01@unicatolica.edu.co>
2022-05-14 10:55:11 -0500

    first commit

---
+++ views/config/users.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <base href="/ayni-local/">
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <title>User</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Nombre</th>
+                            <th>Correo</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>9090</td>
+                            <td>Jose Martinez</td>
+                            <td>Jose@gmail.com</td>
+                        </tr>
+                        <tr>
+                            <td>9091</td>
+                            <td>Carlos Benitez</td>
+                            <td>Carlos@gmail.com</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
