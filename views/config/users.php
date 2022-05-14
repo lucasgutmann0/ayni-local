e080f1d68ff4d5eb52c2d05f230095daa453c497
gutmanndev <lucas.garcia01@unicatolica.edu.co>
2022-05-14 11:00:32 -0500

    uwu

--- views/config/users.php
+++ views/config/users.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <base href="/ayni-local/">
     <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
     <title>User</title>
@@ -36,5 +36,11 @@
             </div>
         </div>
     </div>
+    <script type="text/javascript" src="node_modules/jquery/dist/jquery.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            console.log("Hola")
+        })
+    </script>
 </body>
 </html>
